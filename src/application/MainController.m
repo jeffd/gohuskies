187e1531ed223c6839f3bc646875b86658d857eb
Jeff Dlouhy <jeff.dlouhy@gmail.com>
2009-04-27 13:41:12 -0400

    Removed some whitespace.

--- src/application/MainController.m
+++ src/application/MainController.m
@@ -2,7 +2,7 @@
  * Version: MIT
  *
  * Copyright (c) 2009 Jeff Dlouhy
- * 
+ *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without
@@ -11,10 +11,10 @@
  * copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following
  * conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
  * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -22,7 +22,7 @@
  * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE. 
+ * OTHER DEALINGS IN THE SOFTWARE.
  *
  * Contributor(s):
  *  Jeff Dlouhy <jeff.dlouhy@gmail.com>
@@ -39,7 +39,7 @@
 
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application {
-    
+
     // Add the tab bar controller's current view as a subview of the window
     [mMainWindow addSubview:mTabBarController.view];
 }
