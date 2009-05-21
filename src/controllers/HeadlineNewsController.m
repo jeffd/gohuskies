6d31dc4fc9cd9a3797a938e7f9dece869c14aaad
Jeff Dlouhy <jeff.dlouhy@gmail.com>
2009-05-21 12:16:04 -0400

    Initial work on headlines with custom datasource.

---
+++ src/controllers/HeadlineNewsController.m
@@ -0,0 +1,63 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MIT
+ *
+ * Copyright (c) 2009 Jeff Dlouhy
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Contributor(s):
+ *  Jeff Dlouhy <jeff.dlouhy@gmail.com>
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#import "HeadlineNewsController.h"
+#import "FeedDataSource.h"
+#import "FeedDetailViewController.h"
+
+@implementation HeadlineNewsController
+
+- (void)loadView
+{
+  [super loadView];
+
+  TTNavigationCenter* nav = [TTNavigationCenter defaultCenter];
+  nav.mainViewController = self.navigationController;
+  nav.delegate = self;
+
+  self.navigationBarTintColor = [UIColor colorWithRed:0.8 green:0.0 blue:0.0 alpha:0.0];
+
+  self.tableView = [[[UITableView alloc] initWithFrame:self.view.bounds
+                                        style:UITableViewStylePlain] autorelease];
+  self.tableView.rowHeight = 90;
+	self.tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth
+    | UIViewAutoresizingFlexibleHeight;
+  [self.view addSubview:self.tableView];
+}
+
+- (id<TTTableViewDataSource>)createDataSource {
+  FeedDataSource* dataSource = [[FeedDataSource alloc] initWithFeed:@"http://gonu.com/rss.xml"];
+  [dataSource.delegates addObject:self];
+  [dataSource load:TTURLRequestCachePolicyNoCache nextPage:NO];
+  return dataSource;
+}
+
+@end
